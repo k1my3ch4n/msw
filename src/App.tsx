9a3ad726100edad50110796f40b9faa79594f108
yechan <kevin2695@naver.com>
2024-06-07 07:50:20 +0900

    화면 접속 시 데이터 표출하도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>msw</div>;
+  const [data, setData] = useState(undefined);
+
+  useEffect(() => {
+    fetch("/fake")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data.data);
+      });
+  }, []);
+
+  console.log("data : ", data);
+
+  return <div>{data}</div>;
 };
 
 export default App;
