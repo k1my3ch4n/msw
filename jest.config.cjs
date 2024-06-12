e9d3c4dbb1f1cb179fe6dbcceb668c1e5e05b24a
yechan <kevin2695@naver.com>
2024-06-12 15:36:52 +0900

    jest-html-reporters 추가

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,26 @@
 module.exports = {
   testEnvironment: "jsdom",
-  // testEnvironmentOptions: {
-  //   customExportConditions: [""],
-  // },
+  coverageDirectory: "<rootDir>/reports/coverage/",
+  collectCoverageFrom: [
+    "**/src/**/*.[jt]s?(x)",
+    "!**/src/fixtures/**",
+    "!**/src/mocks/**",
+    "!**/src/*.[jt]s?(x)",
+  ],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./reports",
+        filename: "test_report.html",
+        pageTitle: "Test Report",
+        openReport: true,
+        inlineSource: true,
+        hideIcon: true,
+      },
+    ],
+  ],
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
   clearMocks: true,
 };
