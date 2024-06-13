7debabd7e4eac2f501c1191a3d5d24d5be8f3ead
yechan <kevin2695@naver.com>
2024-06-13 21:09:44 +0900

    msw 셋업

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,9 @@
 import type { Preview } from "@storybook/react";
+import { initialize, mswLoader } from "msw-storybook-addon";
+import { handlers } from '../src/mocks/handlers';
+
+
+initialize({},[...handlers]);
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +14,7 @@ const preview: Preview = {
       },
     },
   },
+  loaders: [mswLoader],
 };
 
 export default preview;
